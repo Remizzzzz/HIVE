c2200c2818ce7fc69d28224a33523bde6699e269
J0coco <msjs.collet@gmail.com>
2024-11-27 20:33:28 +0100

    Tests de git + ajout de commentaires

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -11,16 +11,23 @@ private:
     int j;
 
 public:
+    /** Constructeurs */
     vec2i(int i_, int j_) : i(i_), j(j_){}
     vec2i() : i(0), j(0){}
 
+    /** Constructeur de copie : par défaut */
     vec2i(vec2i const & v_) = default;
+
+    /** Opérateur d'affectation : par défaut */
     vec2i & operator=(vec2i const & v_) = default;
+
     vec2i & operator+=(vec2i const & v_){
         i+= v_.i;
         j+= v_.j;
         return *this;
     };
+
+    /** Comparaison entre vecteurs */
     bool operator==(vec2i const & v_) const {
         return i == v_.i && j == v_.j;
     };
@@ -45,6 +52,7 @@ public:
 
 };
 
+/** Opérations mathématiqes simples */
 vec2i operator+(vec2i const & v1_, vec2i const & v2_);
 vec2i operator-(vec2i const & v1_, vec2i const & v2_);
 vec2i operator*(int const & k, vec2i const & v_);
