25f4ef3e87307e7c9f18faff63dc07d5d84dc648
J0coco <msjs.collet@gmail.com>
2024-11-27 20:09:55 +0100

    Test (g changé une apostrophe)

--- src/features/inputs.h
+++ src/features/inputs.h
@@ -21,7 +21,7 @@ struct Inputs{
     std::string message;
 
     //Si un curseur est en y negatif alors il est sur le deck 1
-    //Si il est en y plus grand que la taille de la map il est sur le deck 2
+    //S'il est en y plus grand que la taille de la map il est sur le deck 2
     //Donc faut bien prendre en compte que ça vise une carte du deck
 
     bool movementNeeded() const{
