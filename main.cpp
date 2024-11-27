29a465bedfa1437df77d52ed7bad55a3fb3f1378
Rémizzz <remi.zongo@etu.utc.fr>
2024-11-27 16:20:10 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include "src/features/map.h"
 #include "src/renderer.h"
-
+#include "src/features/insect.h"
 int main() {
-    /*
     int sideSize = 8; // Taille de la carte
     int rewindLimit = 5;
     Map gameMap(sideSize, rewindLimit);
 
     // Ajouter des insectes pour tester
-    Ant Ant(true,ant);
-    Bee Bee(true,bee);
 
-    gameMap.putInsect(&Ant, vec2i(3, 4));
-    gameMap.putInsect(&Bee, vec2i(2, 2));
+
+    gameMap.putInsectTo(&Ant, vec2i(3, 4));
+    gameMap.putInsectTo(&Bee, vec2i(2, 2));
 
     // Afficher la carte
     Renderer renderer(gameMap);
-    renderer.displayMap();*/
+    renderer.displayMap();
 
     return 0;
 }
