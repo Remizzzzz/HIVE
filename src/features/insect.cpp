f7a433e338a12c50320077f5d7f41a3bc7b4edf0
remia <remi.aerts04@gmail.com>
2024-12-05 14:14:46 +0100

    ajout de Ladybug::getPossibleMovements

--- src/features/insect.cpp
+++ src/features/insect.cpp
@@ -405,6 +405,43 @@ std::vector<vec2i> Mosquitoe:: getPossibleMovements(Map &m) const{
 }
 
 
+std::vector<vec2i> Ladybug::getPossibleMovements(Map &m) const {
+    try{
+        std::vector<vec2i> possibleMovements;
+        // Vérifie que l'insecte puisse bouger
+        if (!this->isLinkingHive(m)) {
+            // 1er niveau de voisins
+            std::list<vec2i> firstLevel = m.getNeighbours(getCoordinates());
+
+            for (const auto &level1 : firstLevel) {
+                // Si le slot contient un pion, on peut monter dessus
+                if (!m.isSlotFree(level1)) {
+                    // 2e niveau de voisins
+                    std::list<vec2i> secondLevel = m.getNeighbours(level1);
+
+                    for (const auto &level2 : secondLevel) {
+                        //on verifie quye les cases du niveau 2 contiennent un pion et q'on ne retourne pas à la case d'origine
+                        if (!m.isSlotFree(level2) && level2 != getCoordinates() && getFormerNeighbour(level2, m) == 1) {
+                            // 3e niveau de voisins
+                            std::list<vec2i> thirdLevel = m.getNeighbours(level2);
+
+                            for (const auto &level3 : thirdLevel) {
+                                // on redescend de la ruche en dernier mouvement, dans une case vide
+                                if (m.isSlotFree(level3) && level3 != level1 && getFormerNeighbour(level3, m) == 1) {
+                                    possibleMovements.push_back(level3);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return possibleMovements;
+    } catch (const HiveException& e) {
+        throw HiveException("Spider::getPossibleMovements", "Erreur dans lafonction pour récupérer les mouvements de Spider");
+    }
+}
+
 
 
 #endif //HIVE_INSECT_CPP
