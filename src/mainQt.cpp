630978fdfda821a1efdc80c8fbb646c8381a2a1d
remia <remi.aerts04@gmail.com>
2024-12-14 12:10:21 +0100

    mainQt.cpp : unused include and use of static method

--- src/mainQt.cpp
+++ src/mainQt.cpp
@@ -1,10 +1,8 @@
 #include <QApplication>
-#include <QPushButton>
-#include "gui/hiverenderer.h"
 #include "gui/mainWindow.h"
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+    return QApplication::exec(); // return a.exec() warning on a -> using static method
 }
