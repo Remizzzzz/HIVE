f5d4f96633552c99d8fdc4c41f2727ab998259ae
Menak49 <146824802+Menak49@users.noreply.github.com>
2024-12-18 15:05:36 +0100

    avancement loadgame

--- src/mainConsole.cpp
+++ src/mainConsole.cpp
@@ -20,6 +20,41 @@ int main() {
     for( auto insect : hive.getInsects()) {
         std::cout << insect->getCoordinates().getI() << std::endl;
     }
+    //hive.displayMenu();
+    hive.loadGame("../hive_parameters.txt");
+    std::cout << std::endl;
+
+    std::cout << "isInit: " << hive.initIfNeeded() << std::endl;
+
+    std::cout << "RewindUsed: " << hive.getRewindUsed() << std::endl;
+    std::cout << "RewindMax: " << hive.getRewindMax() << std::endl;
+
+    // Affichage de l'ensemble des insectes
+    std::cout << "Insects: ";
+    for (auto insect : hive.getInsects()) {
+        std::cout << insect << " ";  // Afficher l'adresse de chaque insecte
+    }
+    std::cout << std::endl;
+
+    // Affichage de l'état du joueur actuel
+    std::cout << "Current Player: " << (hive.getCurrentPlayer() ? "Player 1" : "Player 2") << std::endl;
+
+    // Affichage des coordonnées du joueur
+    std::cout << "Player 1: " << hive.getPlayer1() << std::endl;
+    std::cout << "Player 2: " << hive.getPlayer2() << std::endl;
+
+    // Affichage de l'état du joueur actuel
+    std::cout << "renderer: " << hive.getRenderer() << std::endl;
+    std::cout << "map: " << hive.getMap().getRewind() << std::endl;
+    // Affichage de l'état du joueur actuel
+
+
+
+
+    std::cout << hive.getInsects().at(0)->getCoordinates().getI() << ",";
+    std::cout << hive.getInsects().at(0)->getCoordinates().getI() << std::endl;
+    hive.run();
+    std::cout <<"llaaaa";
     //sleep(10);
     //hive.getRenderer()->displayMap(*hive.getPlayer1());
     //throw HiveException("lala","lala");
