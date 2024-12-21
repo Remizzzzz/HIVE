c4a49e3a42b4bf4ad021880b028c3a9b7c1ed7c0
Menak49 <146824802+Menak49@users.noreply.github.com>
2024-12-21 19:10:24 +0100

    orrection de ant

--- src/features/map.h
+++ src/features/map.h
@@ -134,7 +134,9 @@ public:
      * @param pos_ : position of the slot to check
      */
     bool isSlotFree(const vec2i & pos_) const{
-        return slot[posToIndex(pos_)] == nullptr;
+        int a = posToIndex(pos_);
+        Insect* insec = slot[a];
+        return insec == nullptr;
     }
 
     /**@brief move the insect on pos1_ to the pos2_.*/
