5bf8aa677916f60c16aa4700ce320fcfaac71b8c
J0coco <msjs.collet@gmail.com>
2024-12-22 16:55:16 +0100

    clean utils

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -5,23 +5,23 @@
 #include "utils.h"
 
 
-vec2i operator+(vec2i const & v1_, vec2i const & v2_){
+vec2i operator+(vec2i const & v1_, vec2i const & v2_) {
     return {v1_.getI() + v2_.getI(), v1_.getJ() + v2_.getJ()};
 }
 
-vec2i operator-(vec2i const & v1_, vec2i const & v2_){
+vec2i operator-(vec2i const & v1_, vec2i const & v2_) {
     return {v1_.getI() - v2_.getI(), v1_.getJ() - v2_.getJ()};
 }
 
-vec2i operator*(int const & k, vec2i const & v_){
+vec2i operator*(int const & k, vec2i const & v_) {
     return {k * v_.getI(),v_.getJ()};
 }
 
-bool operator==(vec2i const & v1_, vec2i const & v2_){
+bool operator==(vec2i const & v1_, vec2i const & v2_) {
     return v1_.getI() == v2_.getI() && v1_.getJ() == v2_.getJ();
 }
 
-std::ostream & operator<<(std::ostream & f_, vec2i const & v_){
+std::ostream & operator<<(std::ostream & f_, vec2i const & v_) {
     f_ << '(' << v_.getI() << ',' << v_.getJ() << ')';
     return f_;
 }
